98295569993abd604a51356c1fa2b29cd711c794
rapidgt03 <rapidgt03@naver.com>
2025-05-23 13:21:45 +0900

    1챕터 임시 단어 넣음

--- EngGame/screens/SentenceChap1.cs
+++ EngGame/screens/SentenceChap1.cs
@@ -29,12 +29,39 @@ namespace EngGame.screens
         {
             var wordData = new (string 단어, string 뜻, string 품사)[]
             {
-        ("apple", "사과", "명사"),
-        ("run", "달리다", "동사"),
-        ("beautiful", "아름다운", "형용사"),
-        ("quickly", "빠르게", "부사"),
-        ("think", "생각하다", "동사"),
-        ("이거", "새로", "추가")
+        // 교육 관련 고급 단어
+        ("curriculum", "교육과정", "명사"),
+        ("pedagogy", "교육학, 교수법", "명사"),
+        ("didactic", "교훈적인, 가르치려는", "형용사"),
+        ("syllabus", "강의 계획서, 수업 요강", "명사"),
+        ("tuition", "수업료, 등록금", "명사"),
+        ("extracurricular", "과외의, 정규 교과 외의", "형용사"),
+        ("scholarship", "장학금 / 학문", "명사"),
+        ("proficiency", "숙련, 능숙함", "명사"),
+        ("remedial", "기초 보충의, 교정의", "형용사"),
+        ("assessment", "평가, 시험", "명사"),
+        // 학교 환경 및 제도 관련 단어
+        ("faculty", "교수진", "명사"),
+        ("administration", "행정, 학교 행정부", "명사"),
+        ("matriculation", "입학 (특히 대학)", "명사"),
+        ("commencement", "졸업식", "명사"),
+        ("alumni", "동문, 졸업생들", "명사"),
+        ("disciplinary", "징계의, 규율의", "형용사"),
+        ("enrollment", "등록, 입학 인원", "명사"),
+        ("valedictorian", "졸업생 대표 연설자", "명사"),
+        ("dormitory", "기숙사", "명사"),
+        ("bursar", "재무 담당자 (학교에서의)", "명사"),
+        // 학생과 학습 관련 단어
+        ("diligent", "근면한, 성실한", "형용사"),
+        ("inquisitive", "호기심 많은, 탐구적인", "형용사"),
+        ("articulate", "분명하게 표현하는, 말 잘하는", "형용사"),
+        ("analytical", "분석적인", "형용사"),
+        ("plagiarism", "표절", "명사"),
+        ("collaborative", "협동적인", "형용사"),
+        ("intellect", "지적 능력", "명사"),
+        ("aptitude", "소질, 적성", "명사"),
+        ("attentiveness", "주의 깊음, 집중력", "명사"),
+        ("self-discipline", "자기 절제, 자제력", "명사")
             };
 
             foreach (var (단어, 뜻, 품사) in wordData)
